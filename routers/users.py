5644b204658051fd7a314e87a81c64c6d792c1f4
Tyler <tylerhanson921@gmail.com>
2021-07-14 22:18:38 -0500

    added admin access to routes

--- routers/users.py
+++ routers/users.py
@@ -17,11 +17,18 @@ def disable_user(user_id: int):
     users_controler.disable_user(user_id)
 
 
-@router.post("/users/enable", response_model=List[UserOutput], status_code=200)
+@router.post(
+    "/users/enable",
+    response_model=List[UserOutput],
+    status_code=200,
+    dependencies=[Depends(allow_admin_access)],
+)
 def enable_user(user_id: int):
     users_controler.enable_user(user_id)
 
 
-@router.get("/users/search", status_code=200)
+@router.get(
+    "/users/search", status_code=200, dependencies=[Depends(allow_admin_access)]
+)
 def users_search(limit: int, skip: int):
     return users_controler.search(limit=limit, skip=skip)
