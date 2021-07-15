89f201f7545591f3fbce776f4146a18c67150271
Tyler <tylerhanson921@gmail.com>
2021-07-15 09:53:34 -0500

    better error handling

--- utils/role_checker.py
+++ utils/role_checker.py
@@ -9,5 +9,13 @@ class RoleChecker:
         self.allowed_roles = allowed_roles
 
     def __call__(self, user: UserBase = Depends(get_current_active_user)):
+        if user.roles is None:
+            raise HTTPException(
+                status_code=403,
+                detail="Operation not permitted no role assigned to user",
+            )
         if not any(role in self.allowed_roles for role in user.roles):
-            raise HTTPException(status_code=403, detail="Operation not permitted")
+            raise HTTPException(
+                status_code=403,
+                detail="Operation not permitted user doesnt have correct role",
+            )
